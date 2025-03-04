ae40a0277847643e5d729b25dc4fb51fefa852f8
Artur Zacniewski <a.zacniewski@gmail.com>
2025-03-04 13:37:11 +0100

    Changed STATIC_ROOT settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
 
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
