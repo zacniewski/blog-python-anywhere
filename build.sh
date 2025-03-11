d37ec2b91920ddc92b241e1f4f1238950c75957b
Artur Zacniewski <a.zacniewski@gmail.com>
2025-03-11 11:47:04 +0100

    Try to create superuser in shell

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('testuser', 'testuser@meant4.com', 'test1234')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@meant4.com', 'admin123')"
